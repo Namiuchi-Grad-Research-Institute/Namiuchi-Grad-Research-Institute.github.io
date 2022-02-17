458a0adc4fd90d08a158663e371b8634e58774a6
ms0503 <ms0503@outlook.com>
2022-02-17 23:43:54 +0900

    プロパティを付けたり外したりした。

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header(): JSX.Element {
     return (
         <header id="l-header">
             <div className="header-logo">
-                <img src="https://forum.ngri.jp/res/logo.png" objectFit="contain" layout="fill" alt="NGRIロゴ" />
+                <img src="https://forum.ngri.jp/res/logo.png" alt="NGRIロゴ" />
             </div>
             <nav id="l-navi">
                 <ul className="navi-list">
