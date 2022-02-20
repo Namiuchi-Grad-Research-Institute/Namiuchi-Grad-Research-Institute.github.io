f79198e2b601b13ad9976924aba1432b49b06309
ms0503 <ms0503@outlook.com>
2022-02-21 01:53:20 +0900

    メンバープロフィールページを作成。

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -24,35 +24,35 @@ export default function About(): JSX.Element {
                 <h2 id="member">メンバー</h2>
                 <ul>
                     <li>
-                        <ruby>渡波 空<rt>となみ そら</rt></ruby>
+                        <Link href="/member/渡波+空"><a><ruby>渡波 空<rt>となみ そら</rt></ruby></a></Link>
                         <p>デザインと翻訳以外は基本何でもやってる人。何気に創設者。</p>
                     </li>
                     <li>
-                        ずすめ
+                        <Link href="/member/ずすめ"><a>ずすめ</a></Link>
                         <p>デザインと世界観の出処。ドット絵が得意。</p>
                     </li>
                     <li>
-                        HARU13579
+                        <Link href="/member/HARU13579"><a>HARU13579</a></Link>
                         <p>プログラム担当。Unity勉強中。</p>
                     </li>
                     <li>
-                        free
+                        <Link href="/member/free"><a>free</a></Link>
                         <p>デザイン担当。一応プログラム勉強してる。</p>
                     </li>
                     <li>
-                       <ruby>田中<rt>たなか</rt></ruby> 
+                       <Link href="/member/田中"><a><ruby>田中<rt>たなか</rt></ruby></a></Link>
                         <p>総務部の人。方針決定とかだと思う。</p>
                     </li>
                     <li>
-                        h2o2_n
+                        <Link href="/member/h2o2_n"><a>h2o2_n</a></Link>
                         <p>プログラム担当。多分最年長。</p>
                     </li>
                     <li>
-                        NULL
+                        <Link href="/member/NULL"><a>NULL</a></Link>
                         <p>翻訳担当。本人曰く基本的に英語だが、別言語もやってはみるとのこと。</p>
                     </li>
                     <li>
-                        <ruby>奈良県京都市<rt>ならけんきょうとし</rt></ruby>
+                        <Link href="/member/奈良県京都市"><a><ruby>奈良県京都市<rt>ならけんきょうとし</rt></ruby></a></Link>
                         <p>会社で言う社長的な人。みんなこの人繋がりで集まった。一応人工知能系。</p>
                     </li>
                 </ul>
---
+++ pages/member/index.tsx
@@ -0,0 +1,48 @@
+'use strict';
+
+import Footer from '../components/Footer';
+import Head from 'next/head';
+import Header from '../components/Header';
+import { GetServerSideProps, GetServerSidePropsResult } from 'next';
+import { Image } from '../components/Image';
+import { useRouter } from 'next/router';
+
+type Props = {
+    member: {
+        birthday: string,
+        gender: string[],
+        name: string,
+        role: string[]
+    }
+};
+
+export default function Home({ member }: Props): JSX.Element {
+    return (
+        <div id="l-container">
+            <Head>
+                <title>{member.name} - NGRI</title>
+            </Head>
+            <Header />
+            <main id="l-main">
+                <h1 className="title">{member.name}</h1>
+                <p>性別：女{member.gender[0].substring(1)}% 男{member.gender[0].substring(1)}%</p>
+                <p>誕生日：{member.birthday.split('-')[0]}年{member.birthday.split('-')[1]}月{member.birthday.split('-')[2]}日</p>
+                <p>役職：{role2string(member.role)}</p>
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async (): Promise<GetServerSidePropsResult<{ [key: string]: any; }>> => {
+    const router = useRouter();
+    const { pid } = router.query;
+    const res: Response = await fetch(`https://forum.ngri.jp/api/member/getdata/?name=${pid}`);
+    const member: any = await res.json();
+    return {
+        props: {
+            member
+        }
+    };
+}
+
