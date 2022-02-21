76c4b9c3a024707d21f14a3029e0fbcafef08cf1
ms0503 <ms0503@outlook.com>
2022-02-21 16:30:56 +0900

    メンバープロフィールのシステムをあれこれした。

---
+++ lib/MemberData.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+export function role2str(role: string[]): string {
+    let str: string = '社員';
+    role.map(e => {
+        switch(e) {
+            case 'design2d':
+                str += '・2Dデザイナー';
+                break;
+            case 'design3d':
+                str += '・3Dデザイナー';
+                break;
+            case 'dot2d':
+                str += '・2Dドット絵師';
+                break;
+            case 'dot3d':
+                str += '・3Dドット絵師';
+                break;
+            case 'manage':
+                str += '・総務部';
+                break;
+            case 'soft':
+                str += '・ソフトウェアエンジニア';
+                break;
+            case 'trans':
+                str += '・翻訳者';
+                break;
+            case 'webback':
+                str += '・バックエンドエンジニア';
+                break;
+            case 'webfront':
+                str += '・フロントエンドエンジニア';
+                break;
+            default:
+                str += `・内部名${e}は適切なロールではありません。`;
+                break;
+        }
+    });
+    return str;
+}
+
--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -1,10 +1,11 @@
 'use strict';
 
-import Footer from '../components/Footer';
+import Footer from '../../components/Footer';
 import Head from 'next/head';
-import Header from '../components/Header';
+import Header from '../../components/Header';
 import { GetServerSideProps, GetServerSidePropsResult } from 'next';
-import { Image } from '../components/Image';
+import { Image } from '../../components/Image';
+import { role2str } from '../../lib/MemberData';
 import { useRouter } from 'next/router';
 
 type Props = {
@@ -27,7 +28,7 @@ export default function Home({ member }: Props): JSX.Element {
                 <h1 className="title">{member.name}</h1>
                 <p>性別：女{member.gender[0].substring(1)}% 男{member.gender[0].substring(1)}%</p>
                 <p>誕生日：{member.birthday.split('-')[0]}年{member.birthday.split('-')[1]}月{member.birthday.split('-')[2]}日</p>
-                <p>役職：{role2string(member.role)}</p>
+                <p>役職：{role2str(member.role)}</p>
             </main>
             <Footer />
         </div>
