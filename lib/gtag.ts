2a4be183109ad85c4b99ac8ea48f7b6d33b1d0cd
Sora Tonami <ms0503@outlook.com>
2022-04-11 17:27:32 +0900

    微調整。

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -4,7 +4,7 @@ declare global {
     }
 }
 
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+export const GA_TRACKING_ID = process.env['NEXT_PUBLIC_GA_ID'];
 
 export function pageview(url: string): void {
     if(!GA_TRACKING_ID) return;
