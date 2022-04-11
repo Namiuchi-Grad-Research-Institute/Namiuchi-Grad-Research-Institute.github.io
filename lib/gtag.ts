67b279fe2f22b7852e0e3bd3e29170d4f94feaed
Sora Tonami <ms0503@outlook.com>
2022-04-11 17:45:43 +0900

    微調整。

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -13,7 +13,7 @@ export function pageview(url?: string): void {
     });
 }
 
-export function event({ action, category?, label?, value? }: { action: Gtag.EventNames | string, category?: string, label?: string, value?: number }): void {
+export function event({ action, category, label, value }: { action: Gtag.EventNames | string, category?: string, label?: string, value?: number }): void {
     if(!GA_TRACKING_ID) return;
     window.gtag('event', action, {
         event_category: category,
