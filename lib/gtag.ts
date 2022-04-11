fb2b4c1616ecb8b0843d80c8480d8f58be297dcf
Sora Tonami <ms0503@outlook.com>
2022-04-11 17:39:27 +0900

    微調整。

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -6,14 +6,14 @@ declare global {
 
 export const GA_TRACKING_ID = process.env['NEXT_PUBLIC_GA_ID'];
 
-export function pageview(url: string): void {
+export function pageview(url?: string): void {
     if(!GA_TRACKING_ID) return;
     window.gtag('config', GA_TRACKING_ID, {
         page_path: url
     });
 }
 
-export function event({ action, category, label, value }: {[key: string]: string}): void {
+export function event({ action, category?, label?, value? }: { action: Gtag.EventNames | string, category?: string, label?: string, value?: number }): void {
     if(!GA_TRACKING_ID) return;
     window.gtag('event', action, {
         event_category: category,
