7259e610c9cedf600081cb8d4922a2042e5d9f72
ms0503 <ms0503@outlook.com>
2022-04-21 15:23:33 +0900

    凡ミスを修正。

--- pages/references/index.tsx
+++ pages/references/index.tsx
@@ -15,12 +15,12 @@ export default function Reference(): JSX.Element {
             <main id="l-main">
                 <h1 className="title">リファレンス</h1>
                 <p>当研究所が主に使っているプログラミング言語・マークアップ言語のリファレンス、軽い入門を書いていきます。</p>
-                <ul class="l-grid">
-                    <li class="card"><p><Link href="/references/c/"><a>C言語・C++</a></Link></p></li>
-                    <li class="card"><p><Link href="/references/cs/"><a>C#</a></Link></p></li>
-                    <li class="card"><p><Link href="/references/html/"><a>HTML</a></Link></p></li>
-                    <li class="card"><p><Link href="/references/java/"><a>Java</a></Link></p></li>
-                    <li class="card"><p><Link href="/references/javascript/"><a>JavaScript</a></Link></p></li>
+                <ul className="l-grid">
+                    <li className="card"><p><Link href="/references/c/"><a>C言語・C++</a></Link></p></li>
+                    <li className="card"><p><Link href="/references/cs/"><a>C#</a></Link></p></li>
+                    <li className="card"><p><Link href="/references/html/"><a>HTML</a></Link></p></li>
+                    <li className="card"><p><Link href="/references/java/"><a>Java</a></Link></p></li>
+                    <li className="card"><p><Link href="/references/javascript/"><a>JavaScript</a></Link></p></li>
                 </ul>
             </main>
             <Footer />
