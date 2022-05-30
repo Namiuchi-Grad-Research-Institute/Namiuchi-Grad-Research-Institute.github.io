6d549690c7a30d3b2e7671aea337289525bb7823
ms0503 <ms0503@outlook.com>
2022-05-30 15:59:02 +0900

    一時コミット。

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -3,11 +3,12 @@
 import Footer from '../../components/Footer';
 import Head from 'next/head';
 import Header from '../../components/Header';
-import { GetStaticPathsResult, GetStaticPropsContext, GetStaticPropsResult } from 'next';
+import useSWR from 'swr';
 import { ParsedUrlQuery } from 'querystring';
 import { role2str } from '../../lib/MemberData';
+import { useRouter } from 'next/router';
 
-type Member_t = {
+type Member = {
     birthday: string,
     gender: [string, string, string],
     name: string,
@@ -16,15 +17,46 @@ type Member_t = {
     sp: string
 };
 
-type Props = {
-    member: Member_t
-};
-
-interface Params extends ParsedUrlQuery {
-    name: string
-}
-
-export default function Member({ member }: Props): JSX.Element {
+export default function Member(): JSX.Element {
+    const router = useRouter();
+    const { id, name } = router.query;
+    const queryStr: string = id ? `id=${id}` : `name=${name}`
+    const { data: member, error } = useSWR<Member>(`https://forum.ngri.jp/api/member/getdata/?${queryStr}`, async url => {
+        const res: Response = await fetch(url);
+        return res.json();
+    }, {
+        revalidateOnFocus,
+        revalidateOnReconnect
+    });
+    if(error) {
+        console.error(error);
+        return (
+            <div id="l-container">
+                <Head>
+                    <title>メンバー - NGRI</title>
+                </Head>
+                <Header />
+                <main id="l-main">
+                    <h1 className="title">メンバー</h1>
+                    <p>取得エラーです。</p>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
+    if(!member) return (
+        <div id="l-container">
+            <Head>
+                <title>メンバー - NGRI</title>
+            </Head>
+            <Header />
+            <main id="l-main">
+                <h1 className="title">メンバー</h1>
+                <p>取得中です...</p>
+            </main>
+            <Footer />
+        </div>
+    );
     const birthday: string = member.birthday === null ? '不明/非公開' : `${member.birthday.split('-')[0]}/${member.birthday.split('-')[1]}/${member.birthday.split('-')[2]}`;
     return (
         <div id="l-container">
@@ -34,7 +66,6 @@ export default function Member({ member }: Props): JSX.Element {
             <Header />
             <main id="l-main">
                 <h1 className="title">{member.ruby === null ? member.name : <ruby>{member.name}<rt>{member.ruby}</rt></ruby>}</h1>
-                {/* eslint-disable-next-line no-restricted-properties */}
                 <p>性別：女{member.gender[0].substring(1)}% 男{member.gender[1].substring(1)}% ({getGenderString(member.gender[2])})</p>
                 <p>誕生日(Y/M/D)：{birthday}</p>
                 <p>役職：{role2str(member.role)}</p>
@@ -43,21 +74,6 @@ export default function Member({ member }: Props): JSX.Element {
             <Footer />
         </div>
     );
-}
-
-export async function getStaticPaths(): Promise<GetStaticPathsResult<Params>> {
-    const res: Response = await fetch('https://forum.ngri.jp/api/member/getdata/?name=all');
-    /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */
-    const members: Member_t[] = await res.json();
-    const paths: string[] = members.map((member: Member_t) => `/member/${member.name}`);
-    return { fallback: false, paths };
-}
-
-export async function getStaticProps(ctx: GetStaticPropsContext<Params, false>): Promise<GetStaticPropsResult<Props>> {
-    const res: Response = await fetch(`https://forum.ngri.jp/api/member/getdata/?name=${ctx.params!.name}`);
-    /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */
-    const member: Member_t = await res.json();
-    return { props: { member } };
 }
 
 function getGenderString(gender?: string): string {
@@ -74,8 +90,6 @@ function getGenderString(gender?: string): string {
             return '中性';
         case 'S':
             return '謎';
-        default:
-            break;
     }
     return '？(取得エラー)';
 }
