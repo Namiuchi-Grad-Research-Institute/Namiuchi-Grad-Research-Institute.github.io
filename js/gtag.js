766c868e56b98e2528ba1ab77242a9f8035e926d
ms0503 <ms0503@users.noreply.github.com>
2022-06-02 06:57:10 +0000

    deploy: 23adbb314d37ca152da5143337fd24aa77d94f77

---
+++ js/gtag.js
@@ -0,0 +1,3 @@
+'use strict';
+const GA_TRACKING_ID = 'G-LXW2K15DBT';
+window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config',GA_TRACKING_ID,{page_path:window.location.pathname})
